472348770da8b88628c0be849c59aeef21be9294
Zerlina Jacob <zerlinajv@gmail.com>
2026-02-02 11:39:46 +0530

    Added Unity projects & changed category layout. Project details page made responsive.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function openCategory(category) {
-  var i, tabcontent, tablinks, categorycontent, contentContainer;
-  contentContainer = document.getElementById("categorycontentcontainer");
+  var i, tabcontent, tablinks, category, categorycontent, contentContainer;
+  contentContainer = document.getElementById("categoryContentContainer");
   contentContainer.style.display = "grid"
   tabcontent = document.getElementsByClassName("categorycontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -8,18 +8,20 @@ function openCategory(category) {
   }
   document.getElementById(category).style.display = "grid";
 }
-// if(category == 'All')
-// {
-//   categorycontent = document.getElementsByClassName("categorycontent");
-//   for (i = 0; i < categorycontent.length; i++) {
-//     categorycontent[i].style.display = "grid";
-//   }
-// }
-// else {
-
-// }
-//   elmnt.style.backgroundColor = color;
 
 
-// Get the element with id="defaultOpen" and click on it
-// document.getElementById("defaultOpen").click();
+function changeColor(id) {
+  category = document.getElementsByClassName("category");
+  for (i = 0; i < category.length; i++) {
+    // category[i].style.backgroundColor = "#FFFFFF";
+    category[i].style.backgroundColor = "rgba(145, 200, 228, 0.2)";
+    category[i].style.color = "#000000";
+  }
+    document.getElementById(id).style.backgroundColor = "#333";
+    document.getElementById(id).style.color = "#FFFF";
+}
+
+function wrapper(category, id){
+  openCategory(category);
+  changeColor(id);
+}
